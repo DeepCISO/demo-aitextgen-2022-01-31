1e1a065ae8b5584075803e1416bfc135b0cacf39
Chris Partridge <chris@partridge.tech>
2022-02-01 00:09:00 -0500

    Split data into specific config file, then import

--- run_model.py
+++ run_model.py
@@ -1,8 +1,10 @@
 from aitextgen import aitextgen
+from config import *
+
 
 # With your trained model, you can reload the model at any time by
 # providing the folder containing the pytorch_model.bin model weights + the config, and providing the tokenizer.
-ai = aitextgen(model_folder="trained_model", tokenizer_file="aitextgen.tokenizer.json")
+ai = aitextgen(model_folder=model_folder, tokenizer_file=tokenizer_file)
 
 # ai.generate(10, prompt="log4j is")
 ai.generate(10)
--- train_model.py
+++ train_model.py
@@ -2,29 +2,7 @@ from aitextgen.TokenDataset import TokenDataset
 from aitextgen.tokenizers import train_tokenizer
 from aitextgen.utils import build_gpt2_config
 from aitextgen import aitextgen
-
-# Settings used across tokenization and model configuration
-# https://docs.aitextgen.io/tutorials/model-from-scratch/
-# Remember: model size = vocab_size * embeddings!
-dc_train_from = "EleutherAI/gpt-neo-125M"  # gpt3-like
-dc_vocab_size = 50000  # gpt2 default is 50k, this is ~2GB RAM usage with 1 batch
-dc_max_length = 500
-dc_block_size = dc_max_length  # must always be the same, says docs
-dc_dropout = 0.0
-dc_n_embd = 256
-dc_n_layer = 8
-dc_n_head = 8
-# These all impact speed, and on a GTX 1650 the above == 3.3h/100k steps
-
-# Where to put the files ...
-file_name = "model.txt"  # should be more descriptive in the future
-tokenizer_file = "aitextgen.tokenizer.json"
-
-# Settings used for how long the model is trained
-dc_batch_size = 1  # default was 16?
-dc_num_steps = 1000000
-dc_generate_every = 1000
-dc_save_every = 5000
+from config import *
 
 # Train a custom BPE Tokenizer on the downloaded text
 # This will save one file: `aitextgen.tokenizer.json`, which contains the
